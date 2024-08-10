a7e8017be46dee84415f0acda8f103041e45b697
Urban Tracker <98555033+PaleProwler@users.noreply.github.com>
2024-08-10 18:14:47 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,6 +8,9 @@ rm -rf build/soong;
 git clone https://github.com/ObsidianMaximus/local_manifests.git --depth 1 -b lineage-21 .repo/local_manifests;
 git clone https://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@github.com/PaleProwler/vendor_lineage-priv.git vendor/lineage-priv;
 /opt/crave/resync.sh;
+git clone https://github.com/ObsidianMaximus/android_vendor_lineage.git vendor/lineage;
+git clone https://github.com/ObsidianMaximus/android_frameworks_av.git frameworks/av;
+git clone https://github.com/ObsidianMaximus/android_build_soong.git build/soong;
 source build/envsetup.sh;
 breakfast bitra userdebug;
 brunch bitra
