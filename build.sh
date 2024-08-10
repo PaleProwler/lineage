0f0b39244535ce9d9b25e75b578ceda0fdcd357a
Urban Tracker <98555033+PaleProwler@users.noreply.github.com>
2024-08-10 18:37:09 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ rm -rf vendor/lineage;
 rm -rf frameworks/av;
 rm -rf build/soong;
 git clone https://github.com/ObsidianMaximus/local_manifests.git --depth 1 -b lineage-21 .repo/local_manifests;
-git clone https://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@github.com/PaleProwler/vendor_lineage-priv.git vendor/lineage-priv;
+git clone https://github.com/PaleProwler/vendor_lineage-priv.git vendor/lineage-priv;
 /opt/crave/resync.sh;
 echo "resync content";
 cat /opt/crave/resync.sh;
