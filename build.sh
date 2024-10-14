f6c34372cb195be3185140756fd47128ff660fd3
Urban Tracker <98555033+PaleProwler@users.noreply.github.com>
2024-10-14 12:30:02 +0530

    Build crdroid

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 rm -rf .repo/local_manifests;
 rm -rf vendor/lineage-priv;
-git clone https://github.com/ObsidianMaximus/local_manifests.git --depth 1 -b lineage-21 .repo/local_manifests;
+repo init -u https://github.com/crdroidandroid/android.git -b 14.0 --git-lfs;
+git clone https://github.com/PaleProwler/local_manifests.git --depth 1 -b crdroid-14 .repo/local_manifests;
 /opt/crave/resync.sh;
 source build/envsetup.sh;
 breakfast bitra userdebug;
