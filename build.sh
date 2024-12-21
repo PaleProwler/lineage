890f88ef095e2d6c5db3f03ff9f94a5f7cfda402
Urban Tracker <98555033+PaleProwler@users.noreply.github.com>
2024-12-21 19:48:42 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -2,7 +2,7 @@ rm -rf .repo/local_manifests;
 #rm -rf out;
 rm -rf vendor/lineage-priv;
 repo init -u https://github.com/crdroidandroid/android.git -b 15.0 --git-lfs;
-git clone https://github.com/PaleProwler/local_manifests.git --depth 1 -b crdroid-14 .repo/local_manifests;
+git clone https://github.com/PaleProwler/local_manifests.git --depth 1 -b crDroid-15 .repo/local_manifests;
 /opt/crave/resync.sh;
 source build/envsetup.sh;
 breakfast bitra userdebug;
